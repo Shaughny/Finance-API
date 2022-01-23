c064d53c8ecc849590d0a09697c5dfb5a869d6fb
Mark O'Shaughnessy <mark@pop-os.localdomain>
2022-01-22 23:10:31 -0500

    update cors

--- db.js
+++
@@ -1,9 +0,0 @@
-const dbInfo = {
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'beb00d4502d1f0',
-    password: '9b7de2e2',
-    database: 'heroku_edeb9ea75841df7'
-
-  }
-
-module.exports = { dbInfo};
