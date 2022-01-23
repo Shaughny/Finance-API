d8e51d46d170fcfc1edeade6b36b2a1278189add
Mark O'Shaughnessy <mark@pop-os.localdomain>
2022-01-22 23:13:13 -0500

    update cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
-const dbInfo = require('./db');
+const dbInfo = require('./cleardb');
 
 let db;
 
