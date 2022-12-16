98e677579292efd5cfbc5e03fa08d9390d098060
Mark O'Shaughnessy <shaughnessy.ma@gmail.com>
2022-12-16 10:21:37 -0500

    Update cleardb.js

--- cleardb.js
+++ cleardb.js
@@ -1,8 +1,8 @@
 const dbInfo = {
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'beb00d4502d1f0',
-    password: '9b7de2e2',
-    database: 'heroku_edeb9ea75841df7'
+    host: '****',
+    user: '***',
+    password: '****',
+    database: '**'
 
   }
 
